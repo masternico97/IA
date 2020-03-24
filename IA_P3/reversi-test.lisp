7e6603279036e647cd93c842590747c3c8bc045a
Estudiante Escuela Politecnica Superior <nicolasserranosalas@gmail.com>
2020-03-24 20:54:32 +0100

    mobility-matrix-solution-3-algorith-2

--- IA_P3/reversi-test.lisp
+++ IA_P3/reversi-test.lisp
@@ -24,18 +24,18 @@
   (* (count-difference player board) (mobility player board)))
 
 (defun mix-count-mobility-3 (player board)
-  (let ((count (count-difference player board)))
+  (let ((count (count-difference player board))
         (mov (mobility player board)))
     (if (<= count 0)
         (+ count mov)
-        (* count mov)))
+        (* count mov))))
 
 (defun mix-count-mobility-4 (player board)  ;; Peor que mix-count-mobility-3
-  (let ((count (count-difference player board)))
+  (let ((count (count-difference player board))
         (mov (mobility player board)))
     (if (<= count 0)
         mov
-        count))
+        count)))
 
 (defparameter matrix-solution-1
   '((5 0 1 1 1 1 0 5)
@@ -59,6 +59,16 @@
     (-20 -40 -5 -5 -5 -5 -40 -20)
     (120 -20 20 5  5  20 -20 120)))
 
+(defparameter matrix-solution-3
+  '((4  -3 2  2  2  2  -3 4)
+    (-3 -4 -1 -1 -1 -1 -4 -3)
+    (2  -1 1  0  0  1  -1 2)
+    (2  -1 0  1  1  0  -1 2)
+    (2  -1 0  1  1  0  -1 2)
+    (2  -1 1  0  0  1  -1 2)
+    (-3 -4 -1 -1 -1 -1 -4 -3)
+    (4  -3 2  2  2  2  -3 4)))
+
 (defun get-elemento (player elem-tablero elem-solucion)
   (cond ((equal player elem-tablero) elem-solucion)
         ((equal (opponent player) elem-tablero) (- 0 elem-solucion))
@@ -75,6 +85,9 @@
 
 (defun matrix-solution-2-algorith (player board)  ;; Mejor que matrix-1
   (suma-matriz player (get-board board) matrix-solution-2))
+
+(defun matrix-solution-3-algorith (player board)  ;; Mejor que matrix-1
+  (suma-matriz player (get-board board) matrix-solution-3))
 
 (defun mix-count-mobility-3-matrix-solution-2-algorith-1 (player board)  ;; Peor que matrix-solution-2-algorith 
   (+ (matrix-solution-2-algorith player board) (mix-count-mobility-3 player board)))
@@ -108,24 +121,23 @@
           ((and (< foo 0) (equal bar 0)) foo)
           (t (* foo bar)))))
 
+(defun mobility-matrix-solution-3-algorith-2 (player board)  ;; VICTORIOSO MASTODONTE
+  (let ((foo (matrix-solution-3-algorith player board))
+        (bar (mobility player board)))
+    (cond ((and (< foo 0) (not (equal bar 0))) (/ foo bar))
+          ((and (< foo 0) (equal bar 0)) foo)
+          (t (* foo bar)))))
+
 ;; (reversi #'human (alpha-beta-searcher 2 #'mobility))
 
 ;; (reversi #'random-strategy (alpha-beta-searcher 2 #'matrix-solution-1-algorith))
 
 (round-robin
- (list (alpha-beta-searcher 2 #'mobility)
-       (alpha-beta-searcher 2 #'count-difference)
-       (alpha-beta-searcher 2 #'mix-count-mobility-3)
-       (alpha-beta-searcher 2 #'matrix-solution-2-algorith)
-       (alpha-beta-searcher 2 #'mobility-matrix-solution-2-algorith-2)
-       #'random-strategy)
- 100
+ (list (alpha-beta-searcher 2 #'mobility-matrix-solution-2-algorith-2)
+       (alpha-beta-searcher 2 #'mobility-matrix-solution-3-algorith-2))
+ 1000
  10
- '(mobility
-   count-difference
-   mix-count-mobility-3
-   matrix-solution-2-algorith
-   mobility-matrix-solution-2-algorith-2
-   random-strategy)
+ '(mobility-matrix-solution-2-algorith-2
+   mobility-matrix-solution-3-algorith-2)
  )
 
